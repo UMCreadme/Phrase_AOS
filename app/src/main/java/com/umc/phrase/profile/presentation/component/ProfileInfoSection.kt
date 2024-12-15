468aa9c5d645d4c10374158ad89b7293a2a8ed94
Pearl <rkdwlswn227@gmail.com>
2024-12-15 16:10:33 +0900

    feat(#3): ProfileInfoSection 전체 row, col 재설정 및 preview 수정

--- app/src/main/java/com/umc/phrase/profile/presentation/component/ProfileInfoSection.kt
+++ app/src/main/java/com/umc/phrase/profile/presentation/component/ProfileInfoSection.kt
@@ -16,6 +16,7 @@ import com.umc.phrase.commons.ui.theme.PhraseTheme
 fun ProfileInfoSection(
     profileImage: String,
     profileName: String,
+    profileId: String,
     bio: String,
     booksCount: Int,
     followingCount: Int,
@@ -26,46 +27,87 @@ fun ProfileInfoSection(
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(horizontal = 20.dp, vertical = 20.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // 프로필 사진
-        AsyncImage(
-            model = profileImage,
-            contentDescription = "Profile Image",
-            modifier = Modifier
-                .size(80.dp)
-                .clip(CircleShape)
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            // 프로필 사진
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                AsyncImage(
+                    model = profileImage,
+                    contentDescription = "Profile Image",
+                    modifier = Modifier
+                        .size(66.dp)
+                        .clip(CircleShape)
+                )
+            }
+
+            // 읽은 책, 팔로잉, 팔로워
+            Row(
+                horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 60.dp, end = 20.dp)
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        text = booksCount.toString(),
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+                    Text(text = "읽은 책", style = MaterialTheme.typography.bodyMedium)
+                }
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        text = followingCount.toString(),
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+                    Text(text = "팔로잉", style = MaterialTheme.typography.bodyMedium)
+                }
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        text = followerCount.toString(),
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+                    Text(text = "팔로워", style = MaterialTheme.typography.bodyMedium)
+                }
+            }
+        }
         Spacer(modifier = Modifier.height(8.dp))
 
-        // 프로필 이름
-        Text(text = profileName, style = MaterialTheme.typography.headlineSmall)
-
-        // 소개글
-        Text(
-            text = bio,
-            style = MaterialTheme.typography.bodyMedium,
-            color = Color.Gray,
-            modifier = Modifier.padding(vertical = 4.dp)
-        )
-
-        // 팔로워/팔로잉 수
         Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
             modifier = Modifier.fillMaxWidth()
-        ) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(text = booksCount.toString(), style = MaterialTheme.typography.headlineSmall)
-                Text(text = "읽은 책", style = MaterialTheme.typography.bodyMedium)
-            }
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(text = followingCount.toString(), style = MaterialTheme.typography.headlineSmall)
-                Text(text = "팔로잉", style = MaterialTheme.typography.bodyMedium)
-            }
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(text = followerCount.toString(), style = MaterialTheme.typography.headlineSmall)
-                Text(text = "팔로워", style = MaterialTheme.typography.bodyMedium)
+        ){
+
+            Column(horizontalAlignment = Alignment.Start) {
+                // 프로필 이름
+                Text(
+                    text = profileName,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+
+                // 프로필 ID
+                Text(
+                    text = "@$profileId",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.Gray
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                // 소개글
+                Text(
+                    text = bio,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.Gray
+                )
             }
         }
         Spacer(modifier = Modifier.height(16.dp))
@@ -79,8 +121,10 @@ fun ProfileInfoSection(
                 text = "프로필 편집",
                 onClick = onEditProfileClick,
                 modifier = Modifier.weight(1f),
-                enabled = true,
+                enabled = true
             )
+            Spacer(modifier = Modifier.width(10.dp))
+
             SecondaryButton(
                 text = "프로필 공유",
                 onClick = onShareProfileClick,
@@ -98,10 +142,11 @@ fun ProfileInfoSectionPreview() {
         ProfileInfoSection(
             profileImage = "https://loremflickr.com/150/150",
             profileName = "Pearl",
-            bio = "compose Lover...",
-            booksCount = 11,
-            followingCount = 27,
-            followerCount = 35,
+            profileId = "pearl_k",
+            bio = "compose Lover... It is a test string for long bio example",
+            booksCount = 17,
+            followingCount = 700,
+            followerCount = 777,
             onEditProfileClick = { /* 프로필 편집 로직 추가 */ },
             onShareProfileClick = { /* 프로필 공유 로직 추가 */ }
         )
