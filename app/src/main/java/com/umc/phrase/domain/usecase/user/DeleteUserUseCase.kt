3531195071885f05ec21387850cafbacd4d86276
Pearl <rkdwlswn227@gmail.com>
2025-01-18 14:36:12 +0900

    :sparkles: feat(#7): UserUseCase 구현

--- app/src/main/java/com/umc/phrase/domain/usecase/user/DeleteUserUseCase.kt
+++ app/src/main/java/com/umc/phrase/domain/usecase/user/DeleteUserUseCase.kt
@@ -1,7 +1,12 @@
 package com.umc.phrase.domain.usecase.user
 
-import com.umc.phrase.domain.model.User
+import com.umc.phrase.domain.DataResource
+import com.umc.phrase.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-interface DeleteUserUseCase {
-    suspend operator fun invoke(id: Long): Result<Unit>
+class DeleteUserUseCase @Inject constructor(
+    private val userRepository: UserRepository
+){
+    operator fun invoke(id: Long): Flow<DataResource<Unit>> = userRepository.deleteUser(id)
 }
