3eb94d0a143af46931526a2d24c6bc62d7088576
Pearl <rkdwlswn227@gmail.com>
2025-01-18 14:04:08 +0900

    :sparkles: feat(#7): DataResource 선언 및 UserRepository 함수 설계

--- app/src/main/java/com/umc/phrase/domain/repository/UserRepository.kt
+++ app/src/main/java/com/umc/phrase/domain/repository/UserRepository.kt
@@ -1,10 +1,12 @@
 package com.umc.phrase.domain.repository
 
+import com.umc.phrase.domain.DataResource
 import com.umc.phrase.domain.model.User
+import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
-    suspend fun createUser(user: User): Result<Unit>
-    suspend fun deleteUser(id: Long): Result<Unit>
-    suspend fun getUser(id: Long) : Result<User?>
-    suspend fun updateUser(user: User): Result<Unit>
+    suspend fun createUser(user: User): Flow<DataResource<Unit>>
+    suspend fun deleteUser(id: Long): Flow<DataResource<Unit>>
+    suspend fun getUser(id: Long) : Flow<DataResource<User>>
+    suspend fun updateUser(user: User): Flow<DataResource<Unit>>
 }
